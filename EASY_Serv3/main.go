1951c46e92e49456db7cf0e123bbb6d8349e9f69
maxim <maximzhukov03@mail.ru>
2024-11-18 18:14:16 +0300

    test2

--- EASY_Serv3/main.go
+++ EASY_Serv3/main.go
@@ -6,44 +6,47 @@ import (
 	"net/http"
 )
 
-type User struct{
-	ID int `json:"id"`
-	NAME string `json:"name,omitempty"`
+type User struct {
+	ID    int    `json:"id"`
+	NAME  string `json:"name,omitempty"`
 	EMAIL string `json:"email,omitempty"`
 }
 
-var users = []User{{1, "Petya", "maxmaxiiim2@mal.ruuuuu"}, {2, "Gosha", "wqeaxiiim2@mal.r"}}
+var users = []User{{1, "Petya", "maxmaxiiim2@mal.ruu"}, {2, "Gosha", "wqeaxiiim2@mal.r"}}
 
-func main(){
+func main() {
 	http.HandleFunc("/user", handleUser)
 	http.ListenAndServe("localhost:8080", nil)
 }
 
-func handleUser(w http.ResponseWriter, r *http.Request){
-	switch r.Method{
-	case http.MethodGet: getUser(w, r)
-	case http.MethodPost: postUser(w, r)
-	default: w.WriteHeader(http.StatusBadRequest)
+func handleUser(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getUser(w, r)
+	case http.MethodPost:
+		postUser(w, r)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
 	}
 }
 
-func getUser(w http.ResponseWriter, r *http.Request){
+func getUser(w http.ResponseWriter, r *http.Request) {
 	resp, err := json.Marshal(users)
-	if err != nil{
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	w.Write(resp)
 }
 
-func postUser(w http.ResponseWriter, r *http.Request){
+func postUser(w http.ResponseWriter, r *http.Request) {
 	reqBytes, err := io.ReadAll(r.Body)
-	if err != nil{
+	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	var user User
-	if err = json.Unmarshal(reqBytes, &user); err != nil{
+	if err = json.Unmarshal(reqBytes, &user); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}

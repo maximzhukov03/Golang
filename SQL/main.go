452a8643611dddfb3bf5d6d0d0f4af3b32f10d5a
maxim <maximzhukov03@mail.ru>
2024-11-19 20:45:17 +0300

    update

--- SQL/main.go
+++ SQL/main.go
@@ -4,13 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	 _"github.com/lib/pq"
+
+	_ "github.com/lib/pq"
 )
 
-func main(){
+func main() {
 
 	db, err := sql.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=postgres sslmode=disable password=goLANG")
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
@@ -18,7 +19,7 @@ func main(){
 	if err := db.Ping(); err != nil {
 		log.Fatal(err)
 
-
-		fmt.Println("CONECTED")
 	}
+
+	fmt.Println("CONECTED")
 }

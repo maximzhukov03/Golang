e0e9482224374c77db18a54dbd8aabe9ef48a4e0
maxim <maximzhukov03@mail.ru>
2024-11-23 14:43:17 +0300

    update

--- SQL/main.go
+++ SQL/main.go
@@ -10,16 +10,17 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type User struct{
-	ID int64
-	Name string
-	Email string
-	Password string
+type User struct {
+	ID         int64
+	Name       string
+	Email      string
+	Password   string
 	RegisterAt time.Time
 }
 
 func main() {
-	db, err := sql.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=postgres sslmode=disable password=goLANG")
+	connect := "host=127.0.0.1 port=5432 user=postgres dbname=postgres sslmode=disable password=goLANG" 
+	db, err := sql.Open("postgres", connect)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,36 +34,36 @@ func main() {
 	fmt.Println("CONECTED Наконец то блять")
 
 	rows, err := db.Query("select * from users")
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
 	users := make([]User, 0)
-	for rows.Next(){
+	for rows.Next() {
 		u := User{}
 		err := rows.Scan(&u.ID, &u.Name, &u.Email, &u.Password, &u.RegisterAt)
-		if err != nil{ 
+		if err != nil {
 			log.Fatal(err)
 		}
 		users = append(users, u)
 	}
 
 	err = rows.Err()
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	var u User
-	err = db.QueryRow("select * from users where id = $1", 1).Scan(&u.ID, &u.Name, &u.Email, &u.Password, &u.RegisterAt)
-	if err != nil{
-		if errors.Is(err, sql.ErrNoRows){
+	var us User
+	err = db.QueryRow("select id, name from users where id = $1", 2).Scan(&us.ID, &us.Name)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
 			fmt.Println("no rows")
 			return
 		}
 		log.Fatal(err)
 	}
 
-	fmt.Println(u)
+	fmt.Println(us)
 	fmt.Println(users)
 }

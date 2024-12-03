142d56ac7901935bb09135a3249a9642d2030e8d
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2024-12-03 14:27:09 +0300

    'update'

--- SQL2/main.go
+++ SQL2/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	_ "github.com/lib/pq"
 )
@@ -16,7 +18,14 @@ type User struct{
 	password string
 }
 
+var users = []User{{1, "Mika", "Pedlo", "masdm@maisl.ru", "kjdsakflksjad3j3i2j4o"}}
+
+
 func main(){
+
+	http.HandleFunc("/user", handleUser)
+	http.ListenAndServe("localhost:8080", nil)
+
 	connect := "host=127.0.0.1 port=5432 user=postgres dbname=users_log sslmode=disable password=goLANG"
 	db, err := sql.Open("postgres", connect)
 	if err != nil{
@@ -30,39 +39,55 @@ func main(){
 
 	fmt.Println("CONECTED")
 
-	users, err := GetUsers(db)
-	if err != nil{
-		log.Fatal(err)
-	}
-	fmt.Println(users)
+	// users, err := GetUsers(db)
+	// if err != nil{
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(users)
 
 }
 
-func GetUsers(db *sql.DB) ([]User, error){
-	rows, err := db.Query("SELECT * FROM user_data")
-	if err != nil{
-		log.Fatal(err)
-		return nil, err
+func handleUser(w http.ResponseWriter, r *http.Request){
+	switch r.Method{
+	case http.MethodGet: GetUsers(w, r)
+	default: w.WriteHeader(http.StatusBadRequest)
 	}
-	defer rows.Close()
-
-	users := make([]User, 0)
-	for rows.Next(){
-		u := User{}
-		err := rows.Scan(&u.id, &u.first_name, &u.second_name, &u.email, &u.password)
-		if err != nil{
-			return nil, err
-		}
-		users = append(users, u)
-	}
-
-	err = rows.Err()
-	if err != nil{
-		return nil, err
-	}
-
-	return users, nil
 }
+
+func GetUsers(w http.ResponseWriter, r *http.Request){
+	resp, err := json.Marshal(users)
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(resp)
+}
+
+// func GetUsers(db *sql.DB) ([]User, error){
+// 	rows, err := db.Query("SELECT * FROM user_data")
+// 	if err != nil{
+// 		log.Fatal(err)
+// 		return nil, err
+// 	}
+// 	defer rows.Close()
+
+// 	users := make([]User, 0)
+// 	for rows.Next(){
+// 		u := User{}
+// 		err := rows.Scan(&u.id, &u.first_name, &u.second_name, &u.email, &u.password)
+// 		if err != nil{
+// 			return nil, err
+// 		}
+// 		users = append(users, u)
+// 	}
+
+// 	err = rows.Err()
+// 	if err != nil{
+// 		return nil, err
+// 	}
+
+// 	return users, nil
+// }
 
 func GetUser(db *sql.DB, id int) ([]User, error){
 	rows, err := db.Query("SELECT * FROM user_data where id = $1", id)

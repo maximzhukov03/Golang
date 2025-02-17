d4d97712f5186ac1056816977fcf9ae6bb7adc1b
maxim <maximzhukov03@mail.ru>
2025-02-17 21:53:09 +0300

    update

---
+++ DZ_KATA/go.mod
@@ -0,0 +1,10 @@
+module exemple/golang
+
+go 1.22.3
+
+require github.com/shopspring/decimal v1.4.0
+
+require (
+	github.com/corpix/uarand v0.0.0-20170723150923-031be390f409 // indirect
+	github.com/icrowley/fake v0.0.0-20240710202011-f797eb4a99c0 // indirect
+)
--- DZ_KATA/main.go
+++ DZ_KATA/main.go
@@ -1,77 +1,22 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"github.com/icrowley/fake"
 	"os"
-	"strconv"
-	"strings"
-	"unicode"
 )
 
-type Text struct {
-	Content string
-}
-
-func (t *Text) textModifier() {
-	t.Content = strings.Join(strings.Fields(t.Content), " ")
-	// fmt.Println(t.Content)
-	strHelp := []rune("")
-	runString := []rune(t.Content)
-	for i := 0; i < len(runString); i++ {
-		if runString[i] == '-' {
-			if i > 0 && i < len(runString)-1 {
-				strHelp[len(strHelp)-1], runString[i+1] = runString[i+1], strHelp[len(strHelp)-1]
-			}
-			continue
-		} else {
-			strHelp = append(strHelp, runString[i])
-		}
-	}
-	t.Content = string(strHelp)
-	// fmt.Println(t.Content)
-
-	t.Content = strings.ReplaceAll(t.Content, "+", "!")
-	// fmt.Println(t.Content)
-
-	srezHelp := []int{}
-	strHelp = []rune("")
-	runString = []rune(t.Content)
-	for i := 0; i < len(runString); i++ {
-		if unicode.IsDigit(runString[i]) {
-			intNum, err := strconv.Atoi(string(runString[i]))
-			if err != nil {
-				fmt.Println(err)
-			}
-			srezHelp = append(srezHelp, intNum)
-		} else {
-			strHelp = append(strHelp, runString[i])
-		}
-	}
-	t.Content = strings.Join(strings.Fields(string(strHelp)), " ")
-	sum := 0
-
-	for _, num := range srezHelp {
-		sum += num
-	}
-
-	if sum > 0 {
-		t.Content = fmt.Sprintf("%s %d", t.Content, sum)
-		fmt.Println(t.Content)
-	} else {
-		fmt.Println(t.Content)
-	}
-}
-
 func main() {
-	text := &Text{}
+	fakeGen := GenerateFakeData()
+	fmt.Println(fakeGen)
+	os.Exit(0)
+}
 
-	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Println("Введите строку: ")
-
-	for scanner.Scan() {
-		text.Content = scanner.Text()
-		text.textModifier()
-	}
+func GenerateFakeData() string {
+	Name := fake.FullName()
+	Address := fake.StreetAddress()
+	Phone := fake.Phone()
+	Email := fake.EmailAddress()
+	fakeGen := fmt.Sprintf("Name: %s\nAddress: %s\nPhone: %s\nEmail: %s", Name, Address, Phone, Email)
+	return fakeGen
 }

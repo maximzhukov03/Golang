00b74f6bccfbac0a59908b771ac9b9349468acdc
maxim <maximzhukov03@mail.ru>
2025-03-11 21:00:45 +0300

    update

---
+++ FORKATA/2.package_local/task1.12.2.1/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "goproject/src/go-kata/course1/12.package/2.package_local/task1.12.2.1/test/mymath"
+
+
+
+
+
--- FORKATA/input.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ["$#" -ne 1]; then echo "Module name argument is missing"
-    exit 1
-fi
-
-go mod init $1
-go get github.com/yuin/goldmark
-go mod tidy

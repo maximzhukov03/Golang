1dc481a020553ae5fa9bd01d77cab33a48f2ca88
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-03-12 17:16:21 +0300

    update

--- FORKATA/main.go
+++ FORKATA/main.go
@@ -5,24 +5,52 @@ import (
 	"regexp"
 )
 
+type Ad struct {
+	Title       string
+	Description string
+}
+
 func main() {
-	email := "test@example.com"
-	valid := isValidEmail(email)
-	if valid {
-		fmt.Printf("%s является валидным email-адресом\n", email)
-	} else {
-		fmt.Printf("%s не является валидным email-адресом\n", email)
+	ads := []Ad{
+		{
+			Title:       "Куплю велосипед MeRiDa",
+			Description: "Куплю велосипед meriDA в хорошем состоянии.",
+		},
+		{
+			Title:       "Продам ВаЗ 2101",
+			Description: "Продам ваз 2101 в хорошем состоянии.",
+		},
+		{
+			Title:       "Продам БМВ",
+			Description: "Продам бМв в хорошем состоянии.",
+		},
+		{
+			Title:       "Продам macBook pro",
+			Description: "Продам macBook PRO в хорошем состоянии.",
+		},
+	}
+
+	ads = censorAds(ads, map[string]string{
+		"велосипед merida": "телефон Apple",
+		"ваз":              "ВАЗ",
+		"бмв":              "BMW",
+		"macbook pro":      "Macbook Pro",
+	})
+
+	for _, ad := range ads {
+		fmt.Println(ad.Title)
+		fmt.Println(ad.Description)
+		fmt.Println()
 	}
 }
 
-func isValidEmail(email string) bool {
-	re := regexp.MustCompile(`[@]`)
-	re2 := regexp.MustCompile("\\.com")
-	matches := re.MatchString(email)
-	matches2 := re2.MatchString(email)
-	if matches && matches2{
-		return true
-	} else {
-		return false
+func censorAds(ads []Ad, censor map[string]string) []Ad {
+	for old, new := range censor {
+		for index, _ := range ads{
+			re := regexp.MustCompile(`(?i)` + old)
+			ads[index].Description = re.ReplaceAllString(ads[index].Description, new)
+			ads[index].Title = re.ReplaceAllString(ads[index].Title, new)
+		}
 	}
+	return ads
 }

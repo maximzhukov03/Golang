d8e2668eee5b9faf8e4b10ffca09f717707fb4dc
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-03-13 17:08:40 +0300

    update

--- FORKATA/main.go
+++ FORKATA/main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
-func WriteFile(filePath string, data []byte, perm os.FileMode) error {
-    file, err := os.OpenFile(filePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, perm)
+func WriteFile(data io.Reader, fd io.Writer) error {
+	err := WriteFile(data, fd)
 	if err != nil{
 		return err
 	}
-	file.WriteString(string(data))
 	return nil
 }
 
 
-func main(){
-	err := WriteFile("/path/to/file.txt", []byte("Hello, World!"), os.FileMode(0644))
-	if err != nil {
-	    fmt.Println(err)
-		return
-	}
+func main() {
+    filePath := "course1/13.popular_package/7.package_os/task1.13.7.2/file.txt"
+    // Открываем файл для записи
+    file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+    if err != nil {
+        return
+    }
+    defer file.Close() // отложенная функция закрытия дескриптора файла
+    
+    err = WriteFile(strings.NewReader("Hello, World!"), file)
+    if err != nil {
+        fmt.Println("Ошибка при записи файла:", err)
+    }
 }

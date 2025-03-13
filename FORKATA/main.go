5d35720161e493097ea391fd9854b55611a96acd
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-03-13 15:25:42 +0300

    update

--- FORKATA/main.go
+++ FORKATA/main.go
@@ -2,34 +2,23 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"os"
 )
 
-func isRussianLetter(char rune) bool{
-	if char >= 0x0400 && char <= 0x042F{
-		return true
+func WriteFile(filePath string, data []byte, perm os.FileMode) error {
+    file, err := os.OpenFile(filePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, perm)
+	if err != nil{
+		return err
 	}
-	if char >= 0x0430 && char <= 0x044F{
-		return true
-	}
-	return false
+	file.WriteString(string(data))
+	return nil
 }
 
-func countRussianLetters(s string) map[rune]int {
-    counts := make(map[rune]int)
-	txt := strings.ToLower(s)
-    for _, char := range txt {
-        if isRussianLetter(char) {
-            counts[char] += 1
-        }
-    }
-	
-    return counts
-}
 
-func main() {
-    result := countRussianLetters("Привет, мир!")
-	for key, value := range result {
-        fmt.Printf("%c: %d ", key, value) // в: 1 е: 1 т: 1 м: 1 п: 1 р: 2 и: 2 
-    }
+func main(){
+	err := WriteFile("/path/to/file.txt", []byte("Hello, World!"), os.FileMode(0644))
+	if err != nil {
+	    fmt.Println(err)
+		return
+	}
 }

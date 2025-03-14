a5b7e135b0bbfaf90f208d3ab60de0a2195c9691
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-03-14 17:17:26 +0300

    update

--- FORKATA/main.go
+++ FORKATA/main.go
@@ -8,135 +8,130 @@ import (
 )
 
 type User struct {
-	ID       int
-	Username string
-	Email    string
+    ID       int       `json:"id"`
+    Name     string    `json:"name"`
+    Age      int       `json:"age"`
+    Comments []Comment `json:"comments"`
 }
 
-
+type Comment struct {
+    ID     int    `json:"id"`
+    Text   string `json:"text"`
+    UserID int    `json:"user_id"`
+}
 
 func CreateUserTable() error {
 	db, err := sql.Open("sqlite3", "users.db")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer db.Close()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
 
-	query := `CREATE TABLE IF NOT EXISTS users (ID SERIAL PRIMARY KEY, Username TEXT NOT NULL, Email INT NOT NULL);`
+	query := `CREATE TABLE IF NOT EXISTS users (
+		ID INTEGER PRIMARY KEY AUTOINCREMENT,
+		Name TEXT NOT NULL,
+		Age INTEGER NOT NULL
+	);`
 
 	_, err = db.Exec(query)
-	if err != nil{
+	if err != nil {
+		return err
+	}
+
+	// Создаем таблицу комментариев
+	query = `CREATE TABLE IF NOT EXISTS comments (
+		ID INTEGER PRIMARY KEY AUTOINCREMENT,
+		Text TEXT NOT NULL,
+		UserID INTEGER,
+		FOREIGN KEY (UserID) REFERENCES users(ID) ON DELETE CASCADE
+	);`
+
+	_, err = db.Exec(query)
+	if err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func InsertUser(user User) error {
-	db, err := sql.Open("sqlite3", "users.db")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer db.Close()
+func prepareQuery(operation string, table string, user User) interface{}{
+	sq := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Question)
 
-	query, args, err := PrepareQuery("insert", "users", user)
-	if err != nil{
+	switch operation {
+	case "insert":
+		return sq.Insert(table).Columns("Name", "Age").Values(user.Name, user.Age)
+	case "select":
+		return sq.Select("ID", "Name", "Age").From(table).Where(squirrel.Eq{"ID": user.ID})
+	case "delete":
+		return sq.Delete(table).Where(squirrel.Eq{"ID": user.ID})
+	case "update":
+		return sq.Update(table).Set("Name", user.Name).Set("Age", user.Age).Where(squirrel.Eq{"ID": user.ID})
+	default:
+		return fmt.Errorf(operation)
+	}
+}
+
+func InsertUser(user User) error{
+	db, err := sql.Open("sqlite3", "users.db")
+	if err != nil {
 		return err
 	}
+	defer db.Close()
 
-	_, err = db.Exec(query, args...)
+	_, err = prepareQuery("insert", "users", user).(*squirrel.UpdateBuilder).RunWith(db).Query()
 	if err != nil{
 		return err
 	}
 	return nil
 }
 
-func SelectUser(userID int) (User, error) {
+func SelectUser(userID int) (User, error){
 	db, err := sql.Open("sqlite3", "users.db")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
 
 	user := User{
 		ID: userID,
 	}
-	query, args, err := PrepareQuery("select", "users", user)
-	if err != nil{
-		return user, err
-	}
-	row := db.QueryRow(query, args...)
-	err = row.Scan(&user.ID, &user.Username, &user.Email)
+	_, err = prepareQuery("select", "users", user).(*squirrel.UpdateBuilder).RunWith(db).Query()
 	if err != nil{
 		return user, err
 	}
 	return user, nil
 }
 
-func UpdateUser(user User) error {
+func UpdateUser(user User) error{
 	db, err := sql.Open("sqlite3", "users.db")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer db.Close()
-
-	query, args, err := PrepareQuery("select", "users", user)
-	if err != nil{
-		return err
+	if err != nil {
+		fmt.Println(err)
 	}
-	_, err = db.Exec(query, args...)
+	defer db.Close()
+
+	_, err = prepareQuery("uodate", "users", user).(*squirrel.UpdateBuilder).RunWith(db).Query()
 	if err != nil{
 		return err
 	}
 	return nil
 }
 
-func DeleteUser(userID int) error {
+func DeleteUser(userID int) error{
+	db, err := sql.Open("sqlite3", "users.db")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
 	user := User{
 		ID: userID,
 	}
-	db, err := sql.Open("sqlite3", "users.db")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer db.Close()
-
-	query, args, err := PrepareQuery("select", "users", user)
-	if err != nil{
-		return err
-	}
-	_, err = db.Exec(query, args...)
+	_, err = prepareQuery("delete", "users", user).(*squirrel.UpdateBuilder).RunWith(db).Query()
 	if err != nil{
 		return err
 	}
 	return nil
 }
+func main(){
 
-func PrepareQuery(operation string, table string, user User) (string, []interface{}, error) {
-	sq := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Question)
-
-	switch operation {
-	case "insert":
-		return sq.Insert(table).Columns("Username", "Email").Values(user.Username, user.Email).ToSql()
-
-	case "update":
-		return sq.Update(table).Set("Username", user.Username).Set("Email", user.Email).Where(squirrel.Eq{"ID": user.ID}).ToSql()
-
-	case "delete":
-		return sq.Delete(table).Where(squirrel.Eq{"ID": user.ID}).ToSql()
-
-	case "select":
-		return sq.Select("ID", "Username", "Email").From(table).Where(squirrel.Eq{"ID": user.ID}).ToSql()
-
-	default:
-		return "", nil, fmt.Errorf("неизвестная операция: %s", operation)
-	}
-}
-
-func main() {
-    db, err := sql.Open("sqlite3", "users.db")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer db.Close()
 }

42a67ffbdd1f991c5d7fc4ad5444f51b3936ae8f
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-03-17 18:03:23 +0300

    update

--- FORKATA/main.go
+++ FORKATA/main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	//"fmt"
+	"fmt"
 	"time"
 )
 
-func trySend(ch chan int, v int) bool {
-	select {
-	case ch <- v:
-		return true
-	default:
-		return false
+func timeout(timeout time.Duration) func() bool{
+	ch := make(chan bool, 1)
+	
+	go func(){
+		time.Sleep(timeout)
+		ch <- true
+	}()
 
+	return func() bool{
+		select{
+		case <- ch:
+			return true
+		case <- time.After(timeout):
+			return false
+		}
 	}
-
 }
-
+// Пример использования функции timeout
 func main() {
-	ch := make(chan int, 1)
-	trySend(ch, 48)
-	time.Sleep(1*time.Second)
+    timeoutFunc := timeout(3 * time.Second)
+    since := time.NewTimer(3050 * time.Millisecond)
+    for {
+        select {
+        case <-since.C:
+            fmt.Println("Функция не выполнена вовремя")
+            return
+        default:
+            if timeoutFunc() {
+                fmt.Println("Функция выполнена вовремя")
+                return
+            }
+        }
+    }
 }
+// Output: Функция выполнена вовремя

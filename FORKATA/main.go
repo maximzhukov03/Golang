a7cb86b7709518e50347501ed275b657be8d8a4f
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-03-17 20:20:41 +0300

    update

--- FORKATA/main.go
+++ FORKATA/main.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"os"
+	"time"
+)
+
+var ch = make(chan int)
+
+const (
+	one = iota + 1
+	two
+	three
+	four
+	five
 )
 
 func main() {
-	var wg sync.WaitGroup
+	numbers := []int{one, two, three, four, five}
+	storeNumbers(numbers)
+	print(ch)
+}
 
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			fmt.Println(i)
+func print(data chan int) {
+	if len(os.Getenv("DEBUG")) != 0 {
+		return
+	}
+	go func() {
+		time.Sleep(1 * time.Second)
+		close(ch)
+	}()
+	for v := range data {
+		fmt.Println(v)
+	}
+}
+
+func storeNumbers(numbers []int) {
+	for _, num := range numbers {
+		go func() { // исправить, но не убирать анонимную функцию
+			go write(num)
 		}()
 	}
+}
 
-	wg.Wait()
+func write(n int) {
+	ch <- n
 }

9e86563bfea46034ff2e794ae9a574b993dfb906
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-03-17 17:39:48 +0300

    update

--- FORKATA/main.go
+++ FORKATA/main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"time"
 )
 
-func generateData(n int) chan int {
-	ch := make(chan int, n)
-	for i := 0; i < n; i++ {
-		ch <- i
+func trySend(ch chan int, v int) bool {
+	select {
+	case ch <- v:
+		return true
+	default:
+		return false
+
 	}
-	close(ch)
-	return ch
+
 }
 
 func main() {
-	data := generateData(10)
-	go func() {
-		time.Sleep(1 * time.Second) // ждем одну секунду, чтобы горутина main успела выполниться
-		close(data)
-	}()
-	for num := range data {
-		fmt.Println(num)
-	}
+	ch := make(chan int, 1)
+	trySend(ch, 48)
+	time.Sleep(1*time.Second)
 }

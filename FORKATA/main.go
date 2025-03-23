e1a2fe0ef5ecbd019e8c9062e2b98e63e17d854e
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-03-23 17:51:39 +0300

    update

--- FORKATA/main.go
+++ FORKATA/main.go
@@ -2,30 +2,40 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
 func main() {
 	var res string
-	res = contextWithDeadline(context.Background(), 1*time.Second, 2*time.Second)
-	println(res)
-	res = contextWithDeadline(context.Background(), 2*time.Second, 1*time.Second)
-	println(res)
-	/* Output:
-	context deadline exceeded
-	time after exceeded
-	*/
+	res = contextWithTimeout(context.Background(), 1*time.Second, 2*time.Second)
+	fmt.Println(res)
+	res = contextWithTimeout(context.Background(), 2*time.Second, 1*time.Second)
+	fmt.Println(res)
 }
 
-func contextWithDeadline(ctx context.Context, contextDeadline time.Duration, timeAfter time.Duration) string {
-	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(contextDeadline))
+func contextWithTimeout(ctx context.Context, contextTimeout time.Duration, timeAfter time.Duration) string {
+	ctx, cancel := context.WithTimeout(ctx, contextTimeout)
 	defer cancel()
 	timer := time.NewTimer(timeAfter)
 	defer timer.Stop()
-    select {
-    case <-ctx.Done():
-        return "context deadline exceeded"
-    case <-timer.C:
-        return "time after exceeded"
-    }
+	select{
+	case <-ctx.Done():
+		return "превышено время ожидания"
+	case <-timer.C:
+		return "превышено время ожидания контекста"
+	}
 }
+
+// func contextWithDeadline(ctx context.Context, contextDeadline time.Duration, timeAfter time.Duration) string {
+// 	ctx, cancel := context.WithDeadline(ctx, time.Now().Add(contextDeadline))
+// 	defer cancel()
+// 	timer := time.NewTimer(timeAfter)
+// 	defer timer.Stop()
+//     select {
+//     case <-ctx.Done():
+//         return "context deadline exceeded"
+//     case <-timer.C:
+//         return "time after exceeded"
+//     }
+// }

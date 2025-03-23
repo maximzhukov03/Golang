915134343b467bac8bb9013916733fa832f5175c
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-03-23 19:40:32 +0300

    update

--- FORKATA/main.go
+++ FORKATA/main.go
@@ -1,80 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+	"sync/atomic"
 )
 
+// Пример структуры счетчика
 type Counter struct {
-	mu    sync.Mutex
-	value int
+    count int64
 }
 
-func (c *Counter) Increment() int {
-	c.mu.Lock()
-	c.value++
-	c.mu.Unlock()
-	return c.value
+// Функция для увеличения значения счетчика на 1
+func (c *Counter) Increment() {
+    atomic.AddInt64(&c.count, 1)
 }
 
-func concurrentSafeCounter() int {
-    counter := Counter{}
-    var wg sync.WaitGroup
-	for i := 0; i < 1000; i++ {
-        wg.Add(1)
-		go func() {
-			defer wg.Done()
-            counter.Increment()
-        }()
-    }
-	wg.Wait()
-    return counter.value
+// Функция для получения текущего значения счетчика
+func (c *Counter) GetCount() int64 {
+    return c.count
 }
-
-func main(){
-	a := concurrentSafeCounter()
-	fmt.Println(a)
-}
-
-
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"sync"
-// )
-
-// func waitGroupExample(goroutines ...func() string) string {
-// 	var wg sync.WaitGroup
-// 	res := make(chan string, len(goroutines))
-// 	for _, goRout := range goroutines{
-// 		wg.Add(1)
-// 		go func(goRout func() string){
-// 			defer wg.Done()
-// 			res <- goRout()
-// 		}(goRout)
-// 	}
-// 	wg.Wait()
-// 	close(res)
-
-// 	var stringOutput string
-// 	for i := range res{
-// 		stringOutput += fmt.Sprintf("%s\n", i)
-// 	}
-// 	return stringOutput
-// }
-
-// func main() {
-// 	count := 1000
-// 	goroutines := make([]func() string, count)
-
-// 	for i := 0; i < count; i++ {
-// 		j := i
-// 		goroutines[i] = func() string {
-// 			return fmt.Sprintf("goroutine %d", j)
-// 		}
-// 	}
-
-// 	fmt.Println(waitGroupExample(goroutines...))
-// }

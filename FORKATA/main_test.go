2973d989928b27be1c39d2afe76352e7e6c318cd
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-04-10 19:05:52 +0300

    Update main_test.go

--- FORKATA/main_test.go
+++ FORKATA/main_test.go
@@ -1,34 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	// "fmt"
 	"testing"
+
+	"github.com/google/go-github/v53/github"
+	"github.com/stretchr/testify/assert"
 )
 
-func BenchmarkCrc64(b *testing.B){
-	bench := NewHashMap(16, WithHashCRC64())
-	for i := 0; i < b.N; i++{
-		bench.Set(fmt.Sprintf("key%d", i), i)
+type MockRepoLister struct{}
+type MockGistLister  struct{}
+
+func (mock *MockGistLister) List(ctx context.Context, username string, opt *github.GistListOptions) ([]*github.Gist, *github.Response, error){
+	gitHubRep := []*github.Gist{
+		{Description: github.String("Desc 1"), HTMLURL: github.String("example1@mail.com")},
+		{Description: github.String("Desc 2"), HTMLURL: github.String("example2@mail.com")},
+		{Description: github.String("Desc 3"), HTMLURL: github.String("example3@mail.com")},
 	}
+	return gitHubRep, nil, nil
 }
 
-func BenchmarkCrc32(b *testing.B){
-	bench := NewHashMap(16, WithHashCRC32())
-	for i := 0; i < b.N; i++{
-		bench.Set(fmt.Sprintf("key%d", i), i)
+func (mock *MockRepoLister) List(ctx context.Context, username string, opt *github.RepositoryListOptions) ([]*github.Repository, *github.Response, error){
+	gitHubRep := []*github.Repository{
+		{Name: github.String("Repository 1"), Description: github.String("Desc 1"), HTMLURL: github.String("example1@mail.com")},
+		{Name: github.String("Repository 2"), Description: github.String("Desc 2"), HTMLURL: github.String("example2@mail.com")},
+		{Name: github.String("Repository 3"), Description: github.String("Desc 3"), HTMLURL: github.String("example3@mail.com")},
 	}
+	return gitHubRep, nil, nil
 }
 
-func BenchmarkCrc16(b *testing.B){
-	bench := NewHashMap(16, WithHashCRC16())
-	for i := 0; i < b.N; i++{
-		bench.Set(fmt.Sprintf("key%d", i), i)
+func TestMock(t *testing.T){
+	g := &GithubAdapter{
+		RepoList: &MockRepoLister{},
+		GistList: &MockGistLister{},
 	}
-}
+	TruebodyGist := []Item{{"Desc 1", "TASK: Desc 1", "example1@mail.com"}, {"Desc 2", "TASK: Desc 2", "example2@mail.com"}, {"Desc 3", "TASK: Desc 3", "example3@mail.com"}}
+	TruebodyRep := []Item{{"Repository 1", "Desc 1", "example1@mail.com"}, {"Repository 2", "Desc 2", "example2@mail.com"}, {"Repository 3", "Desc 3", "example3@mail.com"}}
+	bodyGist, err1 := g.GetGists(context.Background(), "ptflp")
+	bodyRepo, err2 := g.GetRepos(context.Background(), "ptflp")
 
-func BenchmarkCrc8(b *testing.B){
-	bench := NewHashMap(16, WithHashCRC8())
-	for i := 0; i < b.N; i++{
-		bench.Set(fmt.Sprintf("key%d", i), i)
-	}
+	assert.Equal(t, bodyGist, TruebodyGist)
+	assert.Equal(t, bodyRepo, TruebodyRep)
+	assert.NoError(t, err1)
+	assert.NoError(t, err2)
+
 }

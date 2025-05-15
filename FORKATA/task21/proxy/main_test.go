becd56d9fc2f94ca961e046a5c5396b62581e365
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-05-15 23:34:56 +0300

    Update main_test.go

--- FORKATA/task21/proxy/main_test.go
+++ FORKATA/task21/proxy/main_test.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"github.com/go-chi/chi"
-	"log"
 	"net/http"
-	"os"
-	"time"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestProxy(t *testing.T){
+	// r := chi.NewRouter()⁡
+	reverseProx := NewReverseProxy("localhost", "1212")
+	handler := reverseProx.ReverseProxy(http.NotFoundHandler())
+
+	req := httptest.NewRequest("GET", "/api", nil)
+	rec := httptest.NewRecorder()
+
+	handler.ServeHTTP(rec, req)
+
+	if rec.Body.String() != "Hello from API"{
+		t.Errorf("ОЖИДАЛОСЬ %s ПОЛУЧИЛИ %s", "Hello from API", rec.Body.String())
+	}
+
+	if rec.Code != 200{
+		t.Errorf("ОЖИДАЛОСЬ %d ПОЛУЧИЛИ %d", 200, rec.Code)
+	}
 
 }

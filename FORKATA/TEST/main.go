e150f41e8d32c32fb0dbf2e2de06edb0a5b834a5
maximzhukov03 <63194496+maximzhukov03@users.noreply.github.com>
2025-06-22 17:46:09 +0300

    update

--- FORKATA/TEST/main.go
+++ FORKATA/TEST/main.go
@@ -1,33 +1,53 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
+	"context"
+	"fmt"
+	"log"
+	"runtime"
+	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
-type List struct{
-	list []int
+func monitorGoroutines(prevGoroutines int) {
+	ticker := time.NewTicker(300 * time.Millisecond)
+	defer ticker.Stop()
+
+	for range ticker.C{
+		countGorout := runtime.NumGoroutine()
+
+		percent := float64(countGorout - prevGoroutines) / float64(prevGoroutines)
+
+		if percent < -0.2{
+			log.Println("⚠️ Предупреждение: Количество горутин уменьшилось более чем на 20%!")
+		}else if percent > 0.2{
+			log.Println("⚠️ Предупреждение: Количество горутин увеличилось более чем на 20%!")
+		}else{
+			log.Printf("Текущее количество горутин: %d\n", countGorout)
+		}
+	}
 }
 
-func main(){
-	// http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "pong")
-	// })
+func main() {	
+	g, _ := errgroup.WithContext(context.Background())
 
-	http.HandleFunc("/ping", JSONhandler)
+	// Мониторинг горутин
+	go func() {
+		monitorGoroutines(runtime.NumGoroutine())
+	}()
 
-	http.ListenAndServe(":8080", nil)
-}
-
-func JSONhandler(w http.ResponseWriter, r *http.Request){
-	list :=  List{
-		list: []int{1, 2, 3, 4},
+	// Имитация активной работы приложения с созданием горутин
+	for i := 0; i < 64; i++ {
+		g.Go(func() error {
+			time.Sleep(5 * time.Second)
+			return nil
+		})
+		time.Sleep(80 * time.Millisecond)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(list)
-	if err != nil{
-		return
+	// Ожидание завершения всех горутин
+	if err := g.Wait(); err != nil {
+		fmt.Println("Ошибка:", err)
 	}
-
 }
